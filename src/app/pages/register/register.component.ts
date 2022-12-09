0e568fd03e8eda14a5ce5acb7b4fcb2f4f5b4b41
Soler Barriocanal, Jordi <Jordi.Soler@gft.com>
2022-12-09 23:10:44 +0100

    feat(register): Register with fake backend

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -22,8 +22,8 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      first_name: ['', [Validators.required, Validators.minLength(3)]],
-      last_name: ['', [Validators.required, Validators.minLength(3)]],
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
       username: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.minLength(6)]],
       password: ['', [Validators.required, Validators.minLength(6)]],
