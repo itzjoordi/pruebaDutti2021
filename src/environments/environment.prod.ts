c9f9646452666b3955633f8dc5d41534b51ebd7c
Soler Barriocanal, Jordi <Jordi.Soler@gft.com>
2022-12-12 08:39:09 +0100

    fix(api): Fix https path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  authAPI: 'http://dutti.netlify.com',
+  authAPI: 'https://dutti.netlify.com',
   swapiAPI: 'https://swapi.dev/api/starships/',
   imagesAPI: 'https://starwars-visualguide.com/assets/img/starships/',
   production: true,
